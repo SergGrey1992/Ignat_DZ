df549f8cfb68804e5905926bf5679312e4dc183b
SergGrey1992 <sergdiag19@gmail.com>
2020-11-09 15:29:25 +0300

    Updates

---
+++ precache-manifest.9f0e10252cdfa59354b5b86bff4d8016.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1415badfdf63080379aad506e51cb57",
+    "url": "/Ignat_DZ/index.html"
+  },
+  {
+    "revision": "2e04c254c89ba2a6fe21",
+    "url": "/Ignat_DZ/static/css/main.7d349501.chunk.css"
+  },
+  {
+    "revision": "f0ef5cfa5321b26bae2c",
+    "url": "/Ignat_DZ/static/js/2.5dcfe4fb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Ignat_DZ/static/js/2.5dcfe4fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e04c254c89ba2a6fe21",
+    "url": "/Ignat_DZ/static/js/main.70adc5ac.chunk.js"
+  },
+  {
+    "revision": "d6b7832bb68c5eaac06d",
+    "url": "/Ignat_DZ/static/js/runtime-main.7649001c.js"
+  }
+]);
