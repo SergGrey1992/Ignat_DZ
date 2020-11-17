a2382dec7f608c6aa484a73093ea68bf94f83cb6
SergGrey1992 <sergdiag19@gmail.com>
2020-11-17 15:06:12 +0300

    DZ 6

--- src/p2-homeworks/h6/HW6.tsx
+++ src/p2-homeworks/h6/HW6.tsx
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import SuperEditableSpan from "./common/c4-SuperEditableSpan/SuperEditableSpan";
 import SuperButton from "../h4/common/c2-SuperButton/SuperButton";
 import {restoreState, saveState} from "./localStorage/localStorage";
+import style from "./HW6.module.css"
 
 function HW6() {
     const [value, setValue] = useState<string>("");
@@ -10,20 +11,25 @@ function HW6() {
         saveState<string>("editable-span-value", value);
     };
     const restore = () => {
-        // setValue();
+
+      if ( value !== null ) {
+
+        setValue(restoreState("editable-span-value", value));
+      }
+
     };
 
     return (
-        <div>
+        <div className={style.wrapperHW6} >
             <hr/>
             homeworks 6
 
             {/*should work (должно работать)*/}
-            <div>
+            <div className={style.wrapper} >
                 <SuperEditableSpan
                     value={value}
                     onChangeText={setValue}
-                    spanProps={{children: value ? undefined : "enter text..."}}
+                    spanProps={{children: value ? undefined : "Напишите текст (Двойной клик)"}}
                 />
             </div>
             <SuperButton onClick={save}>save</SuperButton>
